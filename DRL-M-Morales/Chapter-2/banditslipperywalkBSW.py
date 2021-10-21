5d2645864bca89f7432895dccf6f3784b1fa9e84
daniccast <danielaccaso@gmail.com>
2021-10-20 19:21:15 -0500

    Chapter2 bandit walk

---
+++ DRL-M-Morales/Chapter-2/banditslipperywalkBSW.py
@@ -0,0 +1,33 @@
+'''
+--Actions--
+0 left
+1 right
+
+--------------
+| H | S | G  |
+--------------
+G final state
+S Initial state
+
+'''
+
+P= {
+    0: {   #State- Example state 0
+        0: [(1.0,0,0.0, True)], #Action (Action 0) : Possible transitions for that (State, Action)
+        1: [(1.0,0,0.0, True)]  #[probabilit of that transition, next state, reward, flag thath indicated if the next state is terminal]
+    },
+    1: {
+        0: [ ( 0.8,0,0.0, True ), ( 0.2,2,1.0, True ) ], #Stochastic
+        1: [ ( 0.8,2,1.0, True ), ( 0.2,0,0.0, True ) ]
+    },
+    2: {
+        0: [(1.0,2,0.0, True)],
+        1: [(1.0,2,0.0, True)]
+    }
+}
+
+'''
+Another way:
+import gym, gym_walk
+P= gym.make('BanditSlipperyWalk-v0).env.P
+'''
